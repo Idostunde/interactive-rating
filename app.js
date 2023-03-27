8a191a4567a4649ce6683bfd91be69692874c155
ogedengbe idowu babatunde <idostunde@gmail.com>
2023-03-27 14:01:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btns = document.querySelectorAll("role button");
+const  summitBtn = document.getElementById("submit");
+const feedbackBox = document.getElementById("feedback-box");
+const thankyouBox = document.getElementById('thank-you-box');
+const selectRating = document.getElementById("selected-rating");
+
+
