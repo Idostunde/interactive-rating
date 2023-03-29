119c691dc0e87dc15a5f41a5d013f943e3029c9e
ogedengbe idowu babatunde <idostunde@gmail.com>
2023-03-29 12:27:31 +0100

    my second commit

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const btns = document.querySelectorAll("role button");
-const  summitBtn = document.getElementById("submit");
+const btns = document.querySelector("role button");
+const submitButton = document.getElementById("submit");
 const feedbackBox = document.getElementById("feedback-box");
 const thankyouBox = document.getElementById('thank-you-box');
 const selectRating = document.getElementById("selected-rating");
+const rates = document.querySelector("btn")
 
+submitButton.addEventventListener("click", () => {
+    thankyouBox.classList.remove ("hidden")
+    mainBox.style.display = "none"
+})
 
+rateAgain.addEventventListener("click", () => {
+    thankyouBox.classList.add("hidden")
+    mainBox.style.display = "block"
+})
+
+rates.forEach((rate) => {
+    rate.addEventventListener("click", () => {
+        console.log(rate.innerHTML)
+    })
+})
